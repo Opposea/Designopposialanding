b8ab8fcf6c1d7babecd48c82bc583ebbbcb451dd
Stace28 <35204177+Stace28@users.noreply.github.com>
2025-12-09 21:21:24 +0000

    Dependancies added Logo updated and moved to public folder OpposiaLogoCompact.tsx file changed coding with new logo name and public folder. Changed from imports

--- src/components/OpposiaLogoCompact.tsx
+++ src/components/OpposiaLogoCompact.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import logoImage from 'figma:asset/9f8c54c1094496065f7baa2517c5b6daeda9ab9b.png';
 
 export function OpposiaLogoCompact() {
   return (
     <div className="flex items-center justify-center">
-      <img 
-        src={logoImage} 
-        alt="Opposia Logo" 
+      <img
+        src="/LOGO.png"
+        alt="Opposia Logo"
         className="w-[200px] h-auto lg:w-[280px] drop-shadow-[0_0_30px_rgba(59,130,246,0.4)]"
       />
     </div>
